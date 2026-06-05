555cc547dc9e09d1b0dab6e24eb53b96b5d748d0
marckosalks <m4rcos.sales@gmail.com>
2026-06-05 17:21:56 -0300

    feat: home finalizada, components, navegação

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class HeaderComponent {}
